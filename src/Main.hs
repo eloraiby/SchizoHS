ca46312c09cf31f0ec5c542136d391f55268043d
Wael El Oraiby <wael.eloraiby@gmail.com>
2015-11-10 21:41:17 -0500

    Start with parser

---
+++ src/Main.hs
@@ -0,0 +1,8 @@
+module Main where
+
+import Schizo.Parser
+
+main :: IO ()
+
+main = do
+    putStrLn "Hello World"
---
+++ src/Schizo/Parser.hs
@@ -0,0 +1,15 @@
+module Schizo.Parser where
+
+data Predicate a = Predicate (a -> Bool)
+
+Predicate a ||| Predicate b = Predicate (\x -> a x || b x)
+Predicate a &&& Predicate b = Predicate (\x -> a x && b x)
+
+data Token a = Token [a]
+
+data Match a = Match     ([Token a], [a])
+             | Unmatched ([Token a], [a])
+
+
+
+
