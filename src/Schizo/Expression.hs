34286b268bc4e5b59ef1271a0e3dc38bf5415baa
eloraiby <wael.eloraiby@gmail.com>
2015-11-22 21:36:11 -0500

    - simplify the lookup with chaining

--- src/Schizo/Expression.hs
+++ src/Schizo/Expression.hs
@@ -1,6 +1,6 @@
 module Schizo.Expression where
 
-import Data.Map
+import Data.Map as Map
 
 data SchExp  = Symbol       String
              | Operator     String
@@ -8,11 +8,11 @@ data SchExp  = Symbol       String
              | Tuple        [SchExp]
              | Sequence     [SchExp]
              | Application  (SchExp, [SchExp])
-             | Int64        Integer
+             | Int64        Int
              | Float64      Double
              | String       String
              | Bool         Bool
-             deriving Show
+             deriving (Eq, Show)
 
 data Environment =
     Environment { operators  :: Map String Int       -- infix operators
@@ -22,9 +22,47 @@ data Environment =
                 }
     deriving Show
 
+infixl 9 ?>?
+(?>?) :: Maybe a -> Maybe a -> Maybe a
+
+Just x  ?>? _ = Just x
+Nothing ?>? y = y
+
+sxLookup  :: String
+        -> Environment
+        -> Maybe SchExp
+
+sxLookup s e =   Map.lookup s (expMacros e)
+             ?>? Map.lookup s (privMacros e)
+             ?>? Map.lookup s (impMacros e)
+             ?>? case Map.lookup s (operators e) of
+                     Just i -> Just $ Operator s
+                     Nothing -> Nothing
+
+{-
+-- Macro evaluation
+-}
 eval    :: Environment
         -> SchExp
         -> SchExp
+
 eval env e =
-    error "not implemented yet"
+    case e of
+        Symbol s ->
+            case sxLookup s env of
+                Just v -> eval env v
+                Nothing -> e    -- return the same symbol (unresolved)
+
+        Application  (h, args) -> apply env h args
+        _ -> e
+
+apply   :: Environment
+        -> SchExp
+        -> [SchExp]
+        -> SchExp
+
+apply env e args = error "not implemented"
+
+
+
 
